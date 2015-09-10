466317e8f34ad21820a8ad83fdab73a848e5b368
Graham <graham.perich@gmail.com>
2015-09-10 15:43:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+##Read Me
+This is an event discovery and curation tool for SDSU students. It is built entirely in Rails with Bootstrap for the front-end. 
---
+++ db/migrate/20150910212832_initial_schema.rb
@@ -0,0 +1,24 @@
+class InitialSchema < ActiveRecord::Migration
+  def change
+
+  	create_table :events do |t|
+  		t.text :body
+  		t.timestamps
+  end
+
+  	create_table :categories do |t|
+  		t.string :name
+  end
+  
+ 	 create_table :comments do |t|
+  		t.text :body
+  		t.timestamps
+  end
+
+  	add_reference :comments, :event, index:true
+  	add_reference :comments, :user, index:true
+  	add_reference :events, :user, index:true
+  	add_reference :events, :category, index:true
+
+  end
+end
