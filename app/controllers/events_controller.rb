71c6425c5ae0149320ae6c1e27c6f3cca583e79f
Graham <graham.perich@gmail.com>
2015-09-10 17:19:28 -0700

    added form to create event page; defined EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,5 +4,28 @@ class EventsController < ApplicationController
 
 	end
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		if @event.save
+			redirect_to @event
+		else
+			flash[:danger] = @event.errors.full_messages.to_sentence
+			render :new
+		end
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	private
+
+		def event_params
+			params.require(:event).permit(:body)
+		end
 
 end
