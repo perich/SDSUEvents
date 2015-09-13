e33fcab68beb53d23c3b39e867c1e0e5ee392801
Graham <graham.perich@gmail.com>
2015-09-13 15:04:34 -0700

    added categories and sort by categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,8 @@
 class CategoriesController < ApplicationController
 
+	def show
+		# @category = Category.find(params[:id])
+		@events = Event.where(category_id: params[:id]).order(id: :desc)
+	end
+
 end
