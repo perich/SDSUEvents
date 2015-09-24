2261540c3585c72bbc85f9631a63c1822b3e4610
Graham <graham.perich@gmail.com>
2015-09-23 23:45:33 -0700

    troubleshooting jquery not loading in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.assets.enabled = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
